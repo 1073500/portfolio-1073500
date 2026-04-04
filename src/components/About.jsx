33ef6d8184c05e29e7fc393524fc2890b7bdff12
1073500 <1073500@hr.nl>
2026-04-04 23:07:06 +0200

    styling projects

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ function About() {
                             ABOUT ME
                         </Header>
                     </div>
-                    <div className="text-center">
+                    <div className="text-center p-12">
                         <p className="text-black text-sm text-center">
                             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
                             incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
@@ -20,10 +20,10 @@ function About() {
                         </p>
                     </div>
             </section>
-            <section className="mb-12 flex-col justify-self-start  gap-4 justify-between">
-                <div className="flex flex-col text-start mb-6">
-                    <h1 className="mb-6 text-center">Design</h1>
-                    <div className="grid grid-cols-2 justify-items-center mb-6">
+            <section className="mb-12 justify-self-start gap-4 justify-between">
+                <div className="flex flex-col mb-6">
+                    <h1 className="mb-6 text-end ">Design</h1>
+                    <div className="grid grid-cols-2 sm:items-center justify-items-center sm:flex-col mb-6 text-end">
                         <div
                             className="w-48 h-48 object-contain filter-[drop-shadow(6px_6px_0px_#177642)] relative">
                             {["https://vectorseek.com/wp-content/uploads/2025/07/canva-ai-Logo-PNG-SVG-Vector-01-300x300.png", "https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg", "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/miro-icon.png", "https://iconape.com/wp-content/png_logo_vector/trello-3.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Inkscape_Logo.svg/1280px-Inkscape_Logo.svg.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Adobe_Illustrator_CC_icon.svg/500px-Adobe_Illustrator_CC_icon.svg.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Adobe_Photoshop_CC_icon.svg/960px-Adobe_Photoshop_CC_icon.svg.png",
@@ -52,8 +52,8 @@ function About() {
                     </div>
                 </div>
                 <div className="flex flex-col ">
-                    <h1 className="mb-6 text-center">Development</h1>
-                    <div className="grid grid-cols-2 justify-items-center mb-6">
+                    <h1 className="mb-6 text-end">Development</h1>
+                    <div className="grid grid-cols-2 justify-items-center mb-6 text-end">
                         <div
                             className=" w-48 h-48 object-contain filter-[drop-shadow(6px_6px_0px_#177642)] relative">
                             {["https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/FileZilla_logo.svg/500px-FileZilla_logo.svg.png", "https://code.visualstudio.com/assets/branding/code-stable.png", "https://framerusercontent.com/images/Aiz6UFs2GjtwZCywBWIHZnRAiXY.png?width=200&height=200", "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/postman-icon.png", "https://resources.jetbrains.com/storage/products/company/brand/logos/PhpStorm_icon.svg", "https://dbngin.com/resources/images/apps/dbngin.png", "https://cdn.worldvectorlogo.com/logos/git-bash.svg", // voeg hier meer URLs toe
